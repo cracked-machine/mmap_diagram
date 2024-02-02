9b0b33e00f9176891c62cc7edb565694589559e3
cracked-machine <chrisjsutton@gmail.com>
2024-02-02 04:49:30 +0000

    added first unit tests (mocked cli args)

---
+++ tests/test_mmap.py
@@ -0,0 +1,11 @@
+import mmdiagram.generator
+import unittest 
+import pytest
+
+def test_arg_pair():
+    with unittest.mock.patch('sys.argv', ['mmap_digram.diagram', '0x10']):
+        with pytest.raises(SystemExit):
+            mmdiagram.generator.Diagram()
+
+    with unittest.mock.patch('sys.argv', ['mmap_digram.diagram', '0x10', '0x10']):
+            mmdiagram.generator.Diagram()
